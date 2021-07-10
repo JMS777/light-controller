1d819b23a76ad7e680943b043faa03e0d4ad939a
Jeremy Sapsed <jms737800@gmail.com>
2021-07-10 11:41:49 +0100

    Fiz

--- src/devices/Virtual/AddressableRgbStrip.ts
+++ src/devices/Virtual/AddressableRgbStrip.ts
@@ -13,7 +13,7 @@ import CancellationToken from "../../helpers/CancellationToken";
 import { delay } from "../../helpers/AsyncHelpers";
 import { constrain } from "../../helpers/MathHelper";
 import { DeviceType } from "../../helpers/DeviceType";
-import { Scroll } from "src/models/Effects/AddressableLightEffects";
+import { Scroll } from "../../models/Effects/AddressableLightEffects";
 
 export default class AddressableRgbStrip extends RgbLight implements IAddressableRgbLight {
 
